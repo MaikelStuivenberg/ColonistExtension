82132263dbf46f3b78ba24406e08cf30ee6542f8
Maikel Stuivenberg <maikelstuivenberg@Maikels-MBP.localdomain>
2021-08-01 18:20:51 +0200

    v0.3 - Show last saved games - More (relevant) information per game

--- popup/popup.js
+++ popup/popup.js
@@ -1,65 +1,168 @@
-$(".search_btn").click(function () {
+/** 
+ * Get last saved games from the server
+ */
+$.ajax('https://colonist.maikelstuivenberg.nl/game/last/5', {
+    type: 'GET',
+}).done(function(data) {
+    // Reset table
+    $("#resulttable_last > tbody").empty();
+
+    // Fill table
+    var tbody = $("<tbody />"),
+        tr;
+    $.each(data, function(_, row) {
+        tr = $("<tr class=\"resultrow\" />");
+        // tr.append("<td>" + row.id + "</td>")
+        tr.append("<td width=65>" + moment(row.createdAt).format("DD MMM YY") + "</td>");
+        tr.append("<td width=25>" + moment(row.createdAt).format("HH:mm") + "</td>");
+        var td4 = $("<td width=25><a class=\"details\">Details</a></td>");
+        getPlayers(row).appendTo(tr);
+        td4.appendTo(tr);
+        tr.appendTo(tbody);
+
+        td4.click(function() { showDetailsPart(row) });
+    });
+    tbody.appendTo("#resulttable_last");
+});
+
+/** Toggle search btn on/off (based on input) */
+$(".usr_input").keyup(function() {
+    if ($(".usr_input").val() != '')
+        $(".search_btn").prop('disabled', false);
+    else
+        $(".search_btn").prop('disabled', true);
+});
+
+/**
+ * Search results
+ */
+$(".search_btn").click(function() {
     $("#search").hide();
     $("#search_results").show();
+    $("#search_results_header").text($(".usr_input").val());
 
-    $.ajax('https://colonist.maikelstuivenberg.nl/game?username=' + $(".usr_input").val(), {
+    $.ajax('https://colonist.maikelstuivenberg.nl/game?username=' + encodeURIComponent($(".usr_input").val()), {
         type: 'GET',
-    }).done(function (data) {
+    }).done(function(data) {
 
         // Reset table
         $("#resulttable > tbody").empty();
 
         // Fill table
-        var tbody = $("<tbody />"), tr;
-        $.each(data, function (_, row) {
+        var tbody = $("<tbody />"),
+            tr;
+        $.each(data, function(_, row) {
             tr = $("<tr class=\"resultrow\" />");
-            // $.each(row, function (_, text) {
-            tr.append("<td>" + row.id + "</td>")
-            tr.append("<td>" + moment(row.createdAt).format("MMMM Do YYYY, h:mm:ss a") + "</td>")
-            // });
+            var td1 = $("<td width=65>" + moment(row.createdAt).format("DD MMM YY") + "</td>");
+            var td2 = $("<td width=25>" + moment(row.createdAt).format("HH:mm") + "</td>");
+            var td4 = $("<td width=25><a class=\"details\">Details</a></td>");
+            td1.appendTo(tr);
+            td2.appendTo(tr);
+            getPlayers(row).appendTo(tr);
+            td4.appendTo(tr);
             tr.appendTo(tbody);
 
             // When clicking on this result, show the details 
-            tr.click(function () {
-                // Reset table
-                $("#gametable > tbody").empty();
-
-                // Fill table
-                var dataTbody = $("<tbody />");
-
-                var tr1 = $("<tr><th width=\"150\">Number</th><td>" + row.id + "</td></tr>");
-                tr1.appendTo(dataTbody);
-
-                var tr2 = $("<tr><th>Date</th><td>" + moment(row.createdAt).format("MMMM Do YYYY, h:mm:ss a") + "</td></tr>");
-                tr2.appendTo(dataTbody);
-
-                var parsedJson = JSON.parse(row.json);
-                console.log(parsedJson);
-
-                var tr3 = $("<tr><th>Amt of players</th><td>" + parsedJson.Data.Players.length + "</td></tr>");
-                tr3.appendTo(dataTbody);
-
-                var tr4 = $("<tr><th>Winner</th><td>" + parsedJson.Data.Players.find(el => el.WinningPlayer).Player.UserState.Username + "</td></tr>");
-                tr4.appendTo(dataTbody);
-
-                var tr5 = $("<tr><td colspan=\"2\"><textarea>" + row.json + "</textarea></td></tr>");
-                tr5.appendTo(dataTbody);
-
-
-                dataTbody.appendTo("#gametable");
-
-
-                // Show details & hide search results
-                $("#search_results").hide();
-                $("#game_data").show();
-            });
+            // td1.click(function(){ showDetailsPart(row)});
+            // td2.click(function(){ showDetailsPart(row)});
+            td4.click(function() { showDetailsPart(row) });
         });
         tbody.appendTo("#resulttable");
     });
 });
 
-$(".reset_btn").click(function () {
+$(".reset_btn").click(reset);
+
+function reset() {
+    $(".usr_input").val("");
     $("#search").show();
     $("#search_results").hide();
     $("#game_data").hide();
-});
+}
+
+function getPlayers(row, useDiv = false) {
+    var players;
+    if (useDiv)
+        players = $("<div>");
+    else
+        players = $("<td>");
+
+    for (var i = 0; i < row.players.length; i++) {
+        const usr = row.players[i].username;
+        playersA = $("<a>" + usr + "</a>");
+        playersA.click(function() {
+            reset();
+
+            $(".usr_input").val(usr);
+            $(".search_btn").click();
+        });
+        playersA.appendTo(players);
+
+        if (i < row.players.length - 1) {
+            playersSpan = $("<span>, </span>");
+            playersSpan.appendTo(players);
+        }
+    }
+
+    return players;
+}
+
+function showDetailsPart(row) {
+
+    showGameDetails(row);
+    showDiceStats(row);
+
+    // Show details & hide search results
+    $("#search").hide();
+    $("#resulttable").hide();
+    $("#game_data").show();
+}
+
+function showGameDetails(row) {
+    // Reset table
+    $("#gamedetailstable > tbody").empty();
+
+    // Create new tbody
+    var dataTbody = $("<tbody />");
+
+    var tr1 = $("<tr><th width=\"75\">Number</th><td width=\"325\">" + row.id + "</td></tr>");
+    tr1.appendTo(dataTbody);
+
+    var tr2 = $("<tr><th>Date</th><td>" + moment(row.createdAt).format("DD MMMM YYYY, HH:mm") + "</td></tr>");
+    tr2.appendTo(dataTbody);
+
+    var parsedJson = JSON.parse(row.json);
+
+    // var tr5 = $("<tr><td colspan=\"2\"><textarea>" + row.json + "</textarea></td></tr>");
+    // tr5.appendTo(dataTbody);
+
+    var tr3 = $("<tr><th>Players</th></tr>");
+    var td3 = $("<td class=\"players\">");
+    getPlayers(row, true).appendTo(td3);
+    td3.appendTo(tr3);
+    tr3.appendTo(dataTbody);
+
+    var tr4 = $("<tr><th>Winner</th><td>" + parsedJson.Data.Players.find(el => el.WinningPlayer).Player.UserState.Username + "</td></tr>");
+    tr4.appendTo(dataTbody);
+
+    // Add to table
+    dataTbody.appendTo("#gamedetailstable");
+}
+
+function showDiceStats(row) {
+
+    // Reset table
+    $("#dicestatstable > tbody").empty();
+
+    // Create new tbody
+    var dataTbody = $("<tbody />");
+
+    var parsedJson = JSON.parse(row.json);
+
+    for (var i = 0; i < 11; i++) {
+        dataTbody.append("<tr><th width=\"75\">" + (i + 2) + "</th><td width=\"325\">" + parsedJson.Data.DiceStats[i] + "</td></tr>");
+    }
+
+    // Add to table
+    dataTbody.appendTo("#dicestatstable");
+}
